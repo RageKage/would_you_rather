24cebe4cc8ff62917b7d595a29158e53047c19e1
RageKage <99547837+RageKage@users.noreply.github.com>
2022-10-24 19:36:27 -0500

    commit - 1

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,37 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+<div id="app">
+
+<h1>Would you rather...</h1>
+<would-you-rather 
+v-bind:question = "wryQuestion"
+v-bind:answer1="wryAnswerOne"
+v-bind:answer2 = "wryAnswerTwo"
+@answer-changed = "answerChanged"></would-you-rather>
+<p>{{UserSelectionMessage}}</p>
+</div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import WouldYouRather from './components/WouldYouRather.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    WouldYouRather
+  },
+  data() {
+    return{
+      wryQuestion :'Would you rather never eat cheese again or never drink anything sweet again?',
+      wryAnswerOne : 'No more cheese',
+      wryAnswerTwo: 'No more sweet drinks',
+      UserSelectionMessage: ''
+
+    }
+  },
+  methods: {
+    answerChanged(choice){
+       this.UserSelectionMessage  = `You chose ${choice}`
+    }
   }
 }
 </script>
---
+++ src/components/WouldYouRather.vue
@@ -0,0 +1,62 @@
+<template>
+  <div class="wry">
+
+
+  <h2>Please make your choice</h2>
+
+  <h3>{{ question }}</h3>
+
+
+  <input type="radio"  id="answer1" 
+  v-model="choice"
+  v-bind:value = "answer1"
+  @change="ChoiceMade">
+  <label>{{ answer1 }}</label>
+
+  <input type="radio"   
+  v-model="choice"
+  v-bind:value = "answer2"
+  @change="ChoiceMade">
+  <label>{{ answer2 }}</label>
+  
+  
+
+
+
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'WouldYouRather',
+  props: {
+    question: String,
+    answer1: String,
+    answer2: String,
+  },
+  data() {
+    return {
+      choice : '',
+    }
+  },
+  methods: {
+    ChoiceMade(){
+      this.$emit('answer-changed', this.choice)
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+
+.wry {
+  color: black;
+  border: 1px solid green;
+  border-radius: 5px;
+  background: green;
+}
+</style>
