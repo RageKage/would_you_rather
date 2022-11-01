aa3a0dc57b44ff635a388a9594802c13ed5a05be
RageKage <99547837+RageKage@users.noreply.github.com>
2022-11-01 10:10:46 -0500

    commit - add dist

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,8 @@
 const { defineConfig } = require('@vue/cli-service')
-module.exports = defineConfig({
-  transpileDependencies: true
-})
+module.exports = {
+  publicPath: process.env.NODE_ENV === 'production'
+  // replace with the name of your github repository - just the name, not the https://github.com/you/ part
+    ? '/would_you_rather/'
+    : '/',
+    outputDir: "docs"
+}
